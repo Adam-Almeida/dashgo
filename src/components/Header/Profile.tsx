d0b5413e25c80d7e2b2912cf0d70965efca10341
adam-almeida <adamportifolio@gmail.com>
2022-08-03 23:05:54 -0300

    Refatorando layout responsivo

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,14 +1,21 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfilePros {
+  showProfileData?: boolean;
+}
+
+export function Profile({ showProfileData = true }: ProfilePros) {
   return (
     <Flex align="center">
-      <Box mr="4" textAlign="right">
-        <Text>Adam Almeida</Text>
-        <Text color="gray.300" fontSize="small">
-          adam.designjuridico@gmail.com
-        </Text>
-      </Box>
+      {showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>Adam Almeida</Text>
+          <Text color="gray.300" fontSize="small">
+            adam.designjuridico@gmail.com
+          </Text>
+        </Box>
+      )}
+
       <Avatar
         size="md"
         src="https://github.com/adam-almeida.png"
